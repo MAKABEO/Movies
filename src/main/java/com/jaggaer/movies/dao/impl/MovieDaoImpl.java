d718d401889771e202395bdf43e1af445e846baf
Jesus.Villarraga <jesusantoniovillarraga@gmail.com>
2025-03-17 12:48:07 -0500

    Tech Task 2 - Updated Movie to handle many to many relationship - Updated MovieDaoImpl to use EntityManager

--- src/main/java/com/jaggaer/movies/dao/impl/MovieDaoImpl.java
+++ src/main/java/com/jaggaer/movies/dao/impl/MovieDaoImpl.java
@@ -3,37 +3,50 @@ package com.jaggaer.movies.dao.impl;
 
 import com.jaggaer.movies.dao.interfaces.IMovieDao;
 import com.jaggaer.movies.model.Movie;
+import com.jaggaer.movies.utils.JPAUtil;
+import jakarta.persistence.EntityManager;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MovieDaoImpl implements IMovieDao {
 
-    private final List<Movie> movies = new ArrayList<>();
+    private final EntityManager entityManager;
+
+    public MovieDaoImpl() {
+        this.entityManager = JPAUtil.getEntityManager();
+    }
 
     @Override
-    public void save(Movie entity) {
-        movies.add(entity);
+    public void save(Movie movie) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(movie);
+        entityManager.getTransaction().commit();
     }
 
     @Override
     public Movie findById(int id) {
-        return movies.stream().filter(c -> c.getId() == id).findFirst().orElse(null);
+        return entityManager.find(Movie.class, id);
     }
 
     @Override
     public List<Movie> findAll() {
-        return new ArrayList<>(movies);
+        return entityManager.createQuery("SELECT m FROM Movie m", Movie.class).getResultList();
     }
 
     @Override
-    public void update(Movie entity) {
-        delete(entity.getId());
-        save(entity);
+    public void update(Movie movie) {
+        entityManager.getTransaction().begin();
+        entityManager.merge(movie);
+        entityManager.getTransaction().commit();
     }
 
     @Override
     public void delete(int id) {
-        movies.removeIf(c -> c.getId() == id);
+        entityManager.getTransaction().begin();
+        Movie movie = entityManager.find(Movie.class, id);
+        if (movie != null) {
+            entityManager.remove(movie);
+        }
+        entityManager.getTransaction().commit();
     }
 }
--- src/main/java/com/jaggaer/movies/model/Movie.java
+++ src/main/java/com/jaggaer/movies/model/Movie.java
@@ -4,11 +4,13 @@ import com.jaggaer.movies.model.enums.MovieType;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "movie")
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class Movie {
@@ -22,8 +24,16 @@ public class Movie {
     @Enumerated(EnumType.STRING)
     private MovieType priceType;
 
+    @OneToMany(mappedBy = "movie", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<RentalMovie> rentalMovies;
+
+    public Movie() {
+        this.rentalMovies = new ArrayList<>();
+    }
+
     public Movie(String title, MovieType priceType) {
         this.title = title;
         this.priceType = priceType;
+        this.rentalMovies = new ArrayList<>();
     }
 }
