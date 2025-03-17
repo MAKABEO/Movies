65d57d7a9383d900d2059d123e910b48610c1be7
Jesus.Villarraga <jesusantoniovillarraga@gmail.com>
2025-03-17 12:51:07 -0500

    Tech Task 2 - Updated Main to handle the relationship

--- src/main/java/com/jaggaer/movies/Main.java
+++ src/main/java/com/jaggaer/movies/Main.java
@@ -4,6 +4,7 @@ package com.jaggaer.movies;
 import com.jaggaer.movies.config.FacadeFactory;
 import com.jaggaer.movies.exceptions.MovieRentalException;
 import com.jaggaer.movies.facade.CustomerFacade;
+import com.jaggaer.movies.facade.MovieFacade;
 import com.jaggaer.movies.facade.RentalFacade;
 import com.jaggaer.movies.model.Customer;
 import com.jaggaer.movies.model.Movie;
@@ -18,24 +19,35 @@ public class Main {
         try {
             CustomerFacade customerFacade = FacadeFactory.getCustomerFacade();
             RentalFacade rentalFacade = FacadeFactory.getRentalFacade();
+            MovieFacade movieFacade = FacadeFactory.getMovieFacade();
 
-            Customer customer = new Customer("John Doe");
-            customerFacade.registerCustomer(customer);
+            Customer customer = getOrCreateCustomer(customerFacade, "John Doe");
 
-            List<Rental> rentals = createRentals();
-            rentals.forEach(rental -> rentalFacade.registerRental(customer, rental));
+            Rental rental = createRental();
+            rental.getRentalMovies().forEach(rentalMovie -> movieFacade.registerMovie(rentalMovie.getMovie()));
+            rentalFacade.registerRental(customer, rental);
 
-            System.out.println(StatementGenerator.generateStatement(customer));
+            System.out.println(StatementGenerator.generateStatement(customer,rental));
         } catch (MovieRentalException e) {
             System.err.println("An error occurred: " + e.getMessage());
         }
     }
 
-    private static List<Rental> createRentals() {
-        return List.of(
-                new Rental(new Movie("Zack Snyder's Justice League", MovieType.NEW_RELEASE), 5),
-                new Rental(new Movie("Terminator", MovieType.REGULAR), 1),
-                new Rental(new Movie("Soul", MovieType.CHILDREN), 3)
-        );
+    private static Customer getOrCreateCustomer(CustomerFacade customerFacade, String name) {
+        try {
+            return customerFacade.getCustomerByName(name);
+        } catch (MovieRentalException e) {
+            Customer newCustomer = new Customer(name);
+            customerFacade.registerCustomer(newCustomer);
+            return newCustomer;
+        }
+    }
+
+    private static Rental createRental() {
+        Rental rental = new Rental();
+        rental.addMovie(new Movie("Zack Snyder's Justice League", MovieType.NEW_RELEASE),5);
+        rental.addMovie(new Movie("Terminator", MovieType.REGULAR), 1);
+        rental.addMovie(new Movie("Soul", MovieType.CHILDREN), 3);
+        return rental;
     }
 }
